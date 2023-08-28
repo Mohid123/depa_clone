2b3a70530da25c40ac9e5cbb1d43251188ed3943
fahadulabideen.qt <fahadulabideen.qt@gmail.com>
2023-08-28 11:24:03 +0500

    SubModule Schema update

--- src/models/subModule.model.js
+++ src/models/subModule.model.js
@@ -18,7 +18,7 @@ const subModuleSchema = mongoose.Schema(
       formIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Form', required: true }],
 
       summarySchema: [{ type: String, required: true }],
-      viewSchema: [{ type: String, required: true }],
+      viewSchema: [{ type: Object, required: true }],
       code: { type: String, required: true },
       url: { type: String, required: true },
       title: { type: String },
